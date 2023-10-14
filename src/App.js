974c788bc2a887f64de32343528bdbb00985bf8c
laura_ay <laura.ay1564@gmail.com>
2023-10-13 19:23:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
-function App() {
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 2 } },
+  hidden: { opacity: 0, scale: 0 }
+};
+
+const Box = ({ num }) => {
+
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      className="box"
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+    >
+      <h1>Box {num} </h1>
+    </motion.div>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <box>
+      <motion.div
+      className="box"
+      /**
+       * Setting the initial keyframe to "null" will use
+       * the current value to allow for interruptable keyframes.
+       */
+      whileHover={{ scale: [null, 1.5, 1.4], x: 100 }}
+      transition={{ duration: 0.3 }}
+    >
+      <h1>hover</h1>
+    </motion.div>
+  
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      </box>
+      <Box num={1} />
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <Box num={2} />
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <h1>space</h1>
+      <Box num={3} />
     </div>
   );
 }
-
-export default App;
